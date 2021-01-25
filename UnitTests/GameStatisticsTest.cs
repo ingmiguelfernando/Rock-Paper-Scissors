f21527e827585764387386bb7c9bb4a46e86c1c5
ingmiguelfernando <ing.miguel.fernando@gmail.com>
2021-01-24 19:44:18 -0500

    Final Version with unitTest

---
+++ UnitTests/GameStatisticsTest.cs
@@ -0,0 +1,32 @@
+using System;
+using Rock_Paper_Scissors.Models;
+using Xunit;
+
+namespace UnitTests
+{
+    public class GameStatisticsTest
+    {
+        [Fact]
+        public void Get_Game_Results()
+        {
+            var game = new GameStatistics("usr-1", GameOption.PAPER);
+            var machineOption = game.SelectedOptionMachine;
+
+            if(machineOption == GameOption.PAPER)
+            {
+                Assert.True(game.Tie);
+            }
+
+            if (machineOption == GameOption.ROCK)
+            {
+                Assert.True(game.Win);
+            }
+
+            if (machineOption == GameOption.SCISSORS)
+            {
+                Assert.False(game.Win);
+            }
+
+        }
+    }
+}
